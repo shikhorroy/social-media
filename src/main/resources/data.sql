162337eeeced0ad07ae669e117f98931ac792eb7
Shikhor <shikhor@divine-it.net>
2019-05-10 15:01:26 +0600

    Add Initial DB Data.
    This will populate into db during application start. Required configurations are added into application properties.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO app.sm_config_type (ID, NAME, DESCRIPTION, VALUE, INACTIVE, DELETED) VALUES ('locations', 'Locations', 'Locations', 'Locations', 0, 0);
+
+INSERT INTO app.sm_app_config (ID, SM_CONFIG_TYPE_ID, NAME, DESCRIPTION, INACTIVE, DELETED) VALUES (1, 'locations', 'Sylhet', 'Sylhet', 0, 0);
+INSERT INTO app.sm_app_config (ID, SM_CONFIG_TYPE_ID, NAME, DESCRIPTION, INACTIVE, DELETED) VALUES (2, 'locations', 'Bandarban', 'Bandarban', 0, 0);
+INSERT INTO app.sm_app_config (ID, SM_CONFIG_TYPE_ID, NAME, DESCRIPTION, INACTIVE, DELETED) VALUES (3, 'locations', 'Khulna', 'Khulna', 0, 0);
