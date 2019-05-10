30177425392961e8343b0d84f585ded00549925a
Shikhor <shikhor@divine-it.net>
2019-05-11 04:27:43 +0600

    Find UserDetail frm ID and User Name

--- src/main/java/com/socialmedia/socialmedia/daos/SmUserDetailDao.java
+++ src/main/java/com/socialmedia/socialmedia/daos/SmUserDetailDao.java
@@ -1,9 +1,14 @@
 package com.socialmedia.socialmedia.daos;
 
+import com.socialmedia.socialmedia.models.SmUser;
 import com.socialmedia.socialmedia.models.SmUserDetail;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SmUserDetailDao extends CrudRepository<SmUserDetail, Integer> {
+
+  Optional<SmUserDetail> findByUserIdAndUserUserName(int i, String roy);
 }
